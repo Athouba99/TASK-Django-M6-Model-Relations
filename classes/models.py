953100ac2c44cb2d4484be8658d461767991038e
athouba99 <athooba99@gmail.com>
2022-10-18 19:50:29 +0300

    model relations task

--- classes/models.py
+++ classes/models.py
@@ -1,12 +1,15 @@
+from ctypes.wintypes import tagSIZE
 from django.db import models
 
 
 class Course(models.Model):
     name = models.CharField(max_length=30)
-
+     
+    
 
 class Lecture(models.Model):
     name = models.CharField(max_length=30)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -15,6 +18,7 @@ class Lecture(models.Model):
 class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.name
@@ -23,13 +27,16 @@ class Slide(models.Model):
 class Assignment(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
-
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE, primary_key=True)
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course, related_name="tags")
 
+    class Meta:
+        ordering = ["name"]
     def __str__(self):
         return self.name
