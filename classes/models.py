0a8379c01374c10c5fb45a4ca9c0b59f7d56f37c
athouba99 <athooba99@gmail.com>
2022-10-19 16:13:06 +0300

    models raltion task

--- classes/models.py
+++ classes/models.py
@@ -1,16 +1,29 @@
 from ctypes.wintypes import tagSIZE
+from typing_extensions import Self
+from unicodedata import category
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.TextField()
+
+    def __str__(self) :
+        return self.name , self.description , self.image 
 
 class Course(models.Model):
     name = models.CharField(max_length=30)
-     
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
     
+    def __str__(self):
+        return self.name 
 
+
+    
 class Lecture(models.Model):
     name = models.CharField(max_length=30)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)  # FK act as a cloumn 
     def __str__(self):
         return self.name
 
@@ -19,7 +32,7 @@ class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
     lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE, primary_key=True)
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True) 
     def __str__(self):
         return self.name
 
@@ -28,6 +41,7 @@ class Assignment(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
     lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE, primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True) 
     def __str__(self):
         return self.name
 
@@ -35,8 +49,10 @@ class Assignment(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     courses = models.ManyToManyField(Course, related_name="tags")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
-    class Meta:
-        ordering = ["name"]
     def __str__(self):
         return self.name
+
+
+
